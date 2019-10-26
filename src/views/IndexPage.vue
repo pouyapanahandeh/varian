7d206301751a7e551d38186bf7f16e1ea843695d
John <johnfrom636@gmail.com>
2019-10-26 14:14:15 +0200

    Add index carousel and sample menu page

---
+++ src/views/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <CarouselIndex/>
+</template>
+
+<script>
+import CarouselIndex from '../components/CarouselIndex';
+
+export default {
+	name: 'IndexPage',
+	components: { CarouselIndex }
+};
+</script>
+
+<style scoped>
+
+</style>
